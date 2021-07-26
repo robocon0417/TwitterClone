6dffa1eebedcef6996257171c5948d6087337f6c
robocon <rainbowblues.engl.e650@gmail.com>
2021-07-26 15:40:51 +0900

    その他の画面＆共通化

---
+++ views/common/head.php
@@ -0,0 +1,12 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name='discription' contents='ホーム画面です'>
+<link rel='icon' href="<?php echo HOME_URL; ?>views/img/logo-twitterblue.svg">
+<!-- Bootstrap css-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link href="<?php echo HOME_URL; ?>Views/css/style.css" rel='stylesheet'>
+<!-- JS -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
--- views/profile.php
+++ views/profile.php
@@ -1,39 +1,39 @@
+<?php
+// 設定関連を読み込む
+include_once('../config.php');
+// 便利な関数を読み込む
+include_once('../util.php');
+
+///////////////////////////////////////
+// ツイート一覧
+///////////////////////////////////////
+$view_tweets = [
+    [
+        'user_id' => 1,
+        'user_name' => 'taro',
+        'user_nickname' => '太郎',
+        'user_image_name' => 'sample-person.jpg',
+        'tweet_body' => '今プログラミングをしています。',
+        'tweet_image_name' => null,
+        'tweet_created_at' => '2021-03-15 14:00:00',
+        'like_id' => null,
+        'like_count' => 0,
+    ],
+];
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='icon' href="../views/img/logo-twitterblue.svg">
-    <!-- Bootstrap css-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="../Views/css/style.css" rel='stylesheet'>
-    <!-- JS -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <!-- いいね！JS -->
-    <script src="../Views/js/like.js" defer></script>
+    <?php include_once('../Views/common/head.php'); ?>
     <title>プロフィール画面/Twitterクローン</title>
     <meta name='discription' contents='プロフィール画面です'>
 </head>
 
 <body class="home profile text-center">
     <div class="container">
-        <div class="side">
-            <div class="side-inner">
-                <ul class="nav flex-column">
-                    <li class="nav-item"><a href="home.php" class="nav-link"><img src="../Views/img/logo-twitterblue.svg" alt="" class="icon"></a></li>
-                    <li class="nav-item"><a href="home.php" class="nav-link"><img src="../Views/img/icon-home.svg" alt=""></a></li>
-                    <li class="nav-item"><a href="search.php" class="nav-link"><img src="../Views/img/icon-search.svg" alt=""></a></li>
-                    <li class="nav-item"><a href="notification.php" class="nav-link"><img src="../Views/img/icon-notification.svg" alt=""></a></li>
-                    <li class="nav-item"><a href="profile.php" class="nav-link"><img src="../Views/img/icon-profile.svg" alt=""></a></li>
-                    <li class="nav-item"><a href="post.php" class="nav-link"><img src="../Views/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
-                    <li class="nav-item my-icon"><img src="../Views/img_uploaded/user/sample-person.jpg" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="<a href='profile.php'>プロフィール</a><br><a href='sign-out.php'>ログアウト</a>" data-bs-html="true"></li>
-                </ul>
-            </div>
-        </div>
-        <div class="main">
+    <?php include_once('../Views/common/side.php'); ?>
+    <div class="main">
             <div class="main-header">
                 <h1>太郎</h1>
             </div>
@@ -103,17 +103,20 @@
             <!-- 仕切りエリア-->
             <div class="ditch"></div>
 
-            <!-- ToDo: つぶやき一覧エリア-->
-        
+            <!-- つぶやき一覧エリア -->
+            <?php if (empty($view_tweets)) : ?>
+                <p class="p-3">ツイートがまだありません</p>
+            <?php else : ?>
+                <div class="tweet-list">
+                    <?php foreach ($view_tweets as $view_tweet) : ?>
+                        <?php include('../Views/common/tweet.php') ; ?>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+      
         </div>
     </div>
-    <script>
-      document.addEventListener('DOMContentLoaded',function(){
-          $('.js-popover').popover({
-              container:'body'
-          })
-      },false);
-    </script>
+    <?php include_once('../Views/common/foot.php') ;?>
 </body>
  
 </html>
