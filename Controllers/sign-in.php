2a26a28c5334594c8182196fa7abbbc30f4f88a3
robocon <rainbowblues.engl.e650@gmail.com>
2021-07-31 17:57:12 +0900

    ログイン機能

--- Controllers/sign-in.php
+++ Controllers/sign-in.php
@@ -13,25 +13,20 @@ include_once '../Models/users.php';
 
 //ログイン結果
 $try_login_result = null;
-// var_dump($_POST['email']);
-// var_dump($_POST['password']);
 //メールアドレスとパスワードが入力されている場合
 if (isset($_POST['email']) && isset($_POST['password'])) {
     //ログインチェック実行
-    $user = [];
+    $user = findUserAndCheckPassword($_POST['email'],$_POST['password']);
 
     //ログインに成功した場合
     if ($user) {
-        // var_dump('test');
-        // exit;
         //ユーザー情報をセッションに保存
+        saveUserSession($user);
 
        // ホーム画面へ遷移
         header('Location: ' . HOME_URL . 'Controllers/home.php');
         exit;
     } else {
-        // var_dump('test1');
-        // exit;
         // ログイン失敗
         $try_login_result = false;
     }
